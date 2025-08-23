9eafb17239553f9b879bc7eb08d289a91cd6cda5
mtcodingtech <mtcodingtech@gmail.com>
2025-08-23 13:32:34 +0630

    js

---
+++ GP2/JavaScript/Day1/variables.js
@@ -0,0 +1,32 @@
+// var username = "John";
+// console.log(username);
+
+// let age = 20;
+// age = 30;
+// console.log(age);
+
+// const city = "Ygn";
+// city = "Mdy";
+// console.log(city);
+
+
+// *************************************
+
+// var username = "John";
+
+
+// if(true){
+//     let username = "Rose";
+//     console.log(username)
+// }
+
+
+// console.log(username)
+
+// let name = "John"
+
+//  ********************************
+
+"use strict";
+let num = 5;
+console.log(num)
